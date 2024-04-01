11c2d1b6278149063c9290d499157ef92210435f
SandyTC <138057110+SandyTC@users.noreply.github.com>
2024-04-01 11:45:39 +0100

    Added back option

--- javascript/index.js
+++ javascript/index.js
@@ -68,7 +68,7 @@ function updateCity(event) {
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
     "A"
   )}</small></div>
-  </div>
+  </div><a href="/">Back</a>
   `;
 }
 
